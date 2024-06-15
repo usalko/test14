084cd26c5ebee297406feeee23e6b3b06e58612e
Vanya Usalko <dk4aol@gmail.com>
2024-06-15 23:53:31 +0300

    # There is the fix for a typo. # I've removed unnecessary file.

--- tests/example001.spec.ts
+++ tests/example001.spec.ts
@@ -3,7 +3,7 @@ import { assert } from 'chai'
 import { example001 } from '../src/example001'
 
 describe('#example001', () => {
-    it('Write a JavaScript program to display the current day and time in the following format.', () => {
+    it('Write a TypeScript program to display the current day and time in the following format.', () => {
         const currentDate = new Date()
         const result = example001(currentDate)
         const weekday = Intl.DateTimeFormat('ru-RU', {weekday: 'long'}).format(currentDate)

c11bb9651b808b275f81db5165af119308b41519
Vanya Usalko <dk4aol@gmail.com>
2024-06-23 13:55:49 +0300

    # There is the one more tests for a typescript.

--- src/example003.ts
+++ src/example003.ts
@@ -1,4 +1,22 @@
 // src/example003.ts
 export function example003(date: Date, format: string): string {
-    return ''
+    // TODO: parse format string
+    if (format !== 'mm-dd-yyyy') {
+        throw `Unimplemented format ${format}`
+    }
+    let pad = (n: number, count: number, s: string = '0') => {
+        let result = n.toString()
+        if (result.length > count) {
+            return result.substring(result.length - count)
+        }
+        if (result.length === count) {
+            return result
+        }
+        return s.repeat(count - result.length) + result
+    }
+
+    let months = pad(date.getMonth() + 1, 2)
+    let days = pad(date.getDate(), 2)
+    let years = pad(date.getFullYear(), 4)
+    return [months, days, years].join('-')
 }
--- tests/example003.spec.ts
+++ tests/example003.spec.ts
@@ -7,6 +7,6 @@ describe('#example003', () => {
     it(`Write a JavaScript program to get the current date.
 Expected Output :
 mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy`, () => {
-        expect(() => example003(currentDate, 'mm-dd-yyyy')).equal('')
+        expect(example003(currentDate, 'mm-dd-yyyy')).equal(`${(currentDate.getMonth() + 1).toString().padStart(2, '0')}-${currentDate.getDate().toString().padStart(2, '0')}-${currentDate.getFullYear().toString().padStart(4, '0')}`)
     })
 })

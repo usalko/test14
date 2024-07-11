778132e1161b847a034d50f1bddf7df63dc0fb53
Vanya Usalko <dk4aol@gmail.com>
2024-07-11 10:42:14 +0300

    # There is the next test for typescript.

--- src/example004.ts
+++ src/example004.ts
@@ -1,4 +1,13 @@
 // src/example004.ts
-export function example004(aSide: number, bSide: Number, cSide: number): number {
-    return 0
+
+/*
+    The formula of triangle square.
+    Heron's formula (@see wiki https://en.wikipedia.org/wiki/Area_of_a_triangle)
+    semi-perimeter: s = (a + b + c) / 2
+    area is: ta = sqrt(s * (s - a) * (s - b) * (s - c)
+
+*/
+export function example004(aSide: number, bSide: number, cSide: number): number {
+    const s = (aSide + bSide + cSide) / 2
+    return Math.sqrt(s * (s - aSide) * (s - bSide) * (cSide))
 }
--- tests/example004.spec.ts
+++ tests/example004.spec.ts
@@ -3,7 +3,14 @@ import { assert, expect } from 'chai'
 import { example004 } from '../src/example004'
 
 describe('#example004', () => {
+    // https://www.quora.com/How-do-I-find-the-height-of-a-triangle-given-3-sides
+    // Let triangle ABC with sides: AB = aSide, BC = bSide, AC = cSide
+    // h/BC = AC/AB i.e. h = (AC/AB) * BC == (cSide / aSide) * bSide
+    // triangle area simple formula: h * BC / 2
+    const aSide = 5, bSide = 6, cSide = 7
+    const height = (cSide / aSide) * bSide
+    const triangleArea = height * bSide / 2
     it(`Write a JavaScript program to find the area of a triangle where three sides are 5, 6, 7.`, () => {
-        expect(example004(5, 6, 7)).equal(34)
+        expect(example004(5, 6, 7)).equal(triangleArea)
     })
 })

0bfbb8a2c6c9cd4bf42e82b117d7e274335f20af
Vanya Usalko <dk4aol@gmail.com>
2024-07-11 15:33:05 +0300

    # Thre is the first step for an debug.

--- tests/example004.spec.ts
+++ tests/example004.spec.ts
@@ -5,11 +5,11 @@ import { example004 } from '../src/example004'
 describe('#example004', () => {
     // https://www.quora.com/How-do-I-find-the-height-of-a-triangle-given-3-sides
     // Let triangle ABC with sides: AB = aSide, BC = bSide, AC = cSide
-    // h/BC = AC/AB i.e. h = (AC/AB) * BC == (cSide / aSide) * bSide
+    // h/BC = AB/AC i.e. h = (AB/AC) * BC == (aSide / cSide) * bSide
     // triangle area simple formula: h * BC / 2
     const aSide = 5, bSide = 6, cSide = 7
-    const height = (cSide / aSide) * bSide
-    const triangleArea = height * bSide / 2
+    const height = (aSide / cSide) * bSide
+    const triangleArea = height * cSide / 2
     it(`Write a JavaScript program to find the area of a triangle where three sides are 5, 6, 7.`, () => {
         expect(example004(5, 6, 7)).equal(triangleArea)
     })

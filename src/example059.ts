cdbcb63e16e56c5df63aac88aafdd0df2cf47350
Vanya Usalko <dk4aol@gmail.com>
2024-09-10 14:12:43 +0300

    # There is the new one test.

---
+++ src/example059.ts
@@ -0,0 +1,5 @@
+// src/example059.ts
+
+export function example059(inputString: string): string {
+    return inputString.slice(0, Math.floor(inputString.length / 2))
+}
---
+++ tests/example059.spec.ts
@@ -0,0 +1,18 @@
+// tests/example059.spec.ts
+import { expect } from 'chai'
+import { example059 } from '../src/example059'
+
+console.log(`
+Write a JavaScript program to extract the first half of a even string.
+`)
+describe('#example059', () => {
+    it(`Input string is "Sunny day" #1`, () => {
+        expect(example059('Sunny day')).equal('Sunn')
+    })
+    it(`Input string is "Lovely day" #1`, () => {
+        expect(example059('My heart day')).equal('My hea')
+    })
+    it(`Input string is "" #1`, () => {
+        expect(example059('')).equal('')
+    })
+})

eb711a6223cbb04cbe9bc71d49b9078c0e991fab
Vanya Usalko <dk4aol@gmail.com>
2024-11-23 15:18:23 +0300

    # There is the new one test.

---
+++ src/example133.ts
@@ -0,0 +1,7 @@
+// src/example133.ts
+
+
+export function example133(numerator: number, denominator: number): boolean {
+    return Math.abs(numerator) < Math.abs(denominator)
+}
+
---
+++ tests/example133.spec.ts
@@ -0,0 +1,18 @@
+// tests/example133.spec.ts
+import { expect } from 'chai'
+import { example133 } from '../src/example133'
+
+console.log(`
+Write a JavaScript program to check whether a given fraction is proper or not. 
+Note: There are two types of common fractions, proper or improper. 
+When the numerator and the denominator are both positive, the fraction is called proper
+if the numerator is less than the denominator, and improper otherwise.
+`)
+describe('#example133', () => {
+    it(`Input input fraction is 3/4 #1`, () => {
+        expect(example133(3, 4)).equal(true)
+    })
+    it(`Input input fraction is 4/3 #2`, () => {
+        expect(example133(4, 3)).equal(false)
+    })
+})

3b305cdd6f4e583a453568a6da3f7fe88301fc0f
Vanya Usalko <dk4aol@gmail.com>
2024-11-25 14:21:59 +0300

    # There are an additional implementations for test135.

--- src/example135.ts
+++ src/example135.ts
@@ -1,7 +1,30 @@
 // src/example135.ts
 
 
-export function example135(inputString: string): string {
+export function example135v1(inputString: string): string {
+    let result = ''
+    for (let i = 0; i < inputString.length; i++) {
+        if (inputString.indexOf(inputString[i]) !== inputString.lastIndexOf(inputString[i])) {
+            continue
+        }
+        result += inputString[i]
+    }
+    return result
+}
+
+export function example135v2(inputString: string): string {
+    const inputStringAsArray = inputString.split('')
+    const result = []
+    for (let i = 0; i < inputString.length; i++) {
+        if (inputStringAsArray.indexOf(inputString[i]) !== inputStringAsArray.lastIndexOf(inputString[i])) {
+            continue
+        }
+        result.push(inputString[i])
+    }
+    return result.join('')
+}
+
+export function example135v3(inputString: string): string {
     let uniqueLetters = new Set<string>()
     let nonUniqueLetters = new Set<string>()
     for (let i = 0; i < inputString.length; i++) {
--- tests/example135.spec.ts
+++ tests/example135.spec.ts
@@ -1,6 +1,6 @@
 // tests/example135.spec.ts
 import { expect } from 'chai'
-import { example135 } from '../src/example135'
+import { example135v1, example135v2, example135v3 } from '../src/example135'
 
 console.log(`
 Write a JavaScript program to remove all characters from a given string that appear more than once. 
@@ -8,6 +8,12 @@ Write a JavaScript program to remove all characters from a given string that app
 describe('#example135', () => {
     //abcdefghijklmnopqrstuvwxyz
     it(`Input input string is "avatar" #1`, () => {
-        expect(example135("avatar")).equal("vtr")
+        expect(example135v3("avatar")).equal("vtr")
+    })
+    it(`Input input string is "avatar" #2`, () => {
+        expect(example135v2("avatar")).equal("vtr")
+    })
+    it(`Input input string is "avatar" #3`, () => {
+        expect(example135v1("avatar")).equal("vtr")
     })
 })

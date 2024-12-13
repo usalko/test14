3baf7b8ea8529916cf33d7a4880f84336cd98358
Vanya Usalko <dk4aol@gmail.com>
2024-12-13 12:32:47 +0300

    # There is a little improvement for test.

--- src/example153.ts
+++ src/example153.ts
@@ -2,13 +2,11 @@
 
 
 // convert a comma-separated value (CSV) string to a 2D array
-export function example153(inputString: string): any[][] {
-    const lineSeparator = '\n'
-    const fieldSeparator = ','
-    const result: any[][] = Array()
-    inputString.split(lineSeparator).forEach((line: string) => {
-        result.push(line.split(fieldSeparator))
-    })
-    return result
+export function example153(inputString: string, omitHeader: boolean = false, lineSeparator: string = '\n', fieldSeparator: string = ','): any[][] {
+    return inputString.split(lineSeparator)
+        .filter((line: string, index: number) => !omitHeader || index > 0)
+        .map((line: string) =>
+            line.split(fieldSeparator))
+
 }
 

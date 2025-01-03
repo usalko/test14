b80dbb79fe1085778b355ac5efe745f81e31a60e
Vanya Usalko <dk4aol@gmail.com>
2025-01-03 11:24:20 +0300

    # There is another implementation of test 174.

--- src/example174.ts
+++ src/example174.ts
@@ -5,3 +5,7 @@
 export function example174<T>(inputString: string): string {
     return inputString.slice(0, 1).toLowerCase() + inputString.slice(1)
 }
+
+export function example174v2<T>([firstChar, ...nextChars]: string[], upperCaseRest: boolean = false): string {
+    return firstChar.toLowerCase() + (upperCaseRest ? nextChars.map((ch) => ch.toUpperCase()).join(''): nextChars.join(''))
+}
--- tests/example174.spec.ts
+++ tests/example174.spec.ts
@@ -1,6 +1,6 @@
 // tests/example174.spec.ts
 import { expect } from 'chai'
-import { example174 } from '../src/example174'
+import { example174, example174v2 } from '../src/example174'
 
 console.log(`Fundamental(ES6)-Part-I
 24. De-capitalize First Letter of String
@@ -10,5 +10,8 @@ Write a JavaScript program to de-capitalize the first letter of a string.
 describe('#example174', () => {
     it(`Input string is "Hello world!" #1`, () => {
         expect(example174("Hello world!")).equal("hello world!")
+    })
+    it(`Input string is "Good buy world!" #2`, () => {
+        expect(example174v2("Good buy world!", true)).equal("gOOD BUY WORLD!")
     })
 })

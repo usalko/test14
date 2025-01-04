a70cc376f6d8b5b48da14d374ed50225b1ad4f96
Vanya Usalko <dk4aol@gmail.com>
2025-01-04 11:57:09 +0300

    # There is the new one test.

--- src/example175.ts
+++ src/example175.ts
@@ -2,6 +2,6 @@
 
 
 // Create an array out of the two arrays by creating each possible pair from the two arrays.
-export function example175<T1, T2>(inputArray1: T1[], inputArray2: T2[]): Array<Array<[T1, T2]>> {
-    return []
+export function example175<T1, T2>(inputArray1: T1[], inputArray2: T2[]): [T1, T2][] {
+    return inputArray1.map((x) => inputArray2.map((y) => [x, y]) as [T1, T2][]).flat()
 }

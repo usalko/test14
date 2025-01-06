8879a0c6d70bd826271e434d28d177bdcc345fe9
Vanya Usalko <dk4aol@gmail.com>
2025-01-06 06:58:01 +0300

    # There is the new one task.

---
+++ src/example177.ts
@@ -0,0 +1,8 @@
+// src/example176.ts
+
+
+// Find every element in any of the two given arrays at once,
+// using the provided comparator function.
+export function example176(inputString: string): boolean {
+    return true
+}
---
+++ tests/example177.spec.ts
@@ -0,0 +1,15 @@
+// tests/example177.spec.ts
+import { expect } from 'chai'
+import { example177 } from '../src/example177'
+
+console.log(`Fundamental(ES6)-Part-I
+27. Find Common Elements with Comparator
+
+find every element in any of the two given arrays at once,
+using the provided comparator function.Write a JavaScript program to 
+`)
+describe('#example177', () => {
+    it(`Input string is "y" #1`, () => {
+        expect(example177("y")).equal(true)
+    })
+})

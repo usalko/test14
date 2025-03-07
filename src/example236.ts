fee6cfd86f52a9816cb16f0b3bce84efb3bda919
Vanya Usalko <dk4aol@gmail.com>
2025-03-08 01:17:56 +0300

    # There is the fix type assertion.

--- src/example236.ts
+++ src/example236.ts
@@ -1,7 +1,7 @@
 // src/example236.ts
 
 // Write a JavaScript program to get the maximum value of an array, after mapping each element to a value using the provided function.
-export function example236(inputArray: any[], inputFunctionOrKey: (x: any) => any | string): any {
+export function example236(inputArray: any[], inputFunctionOrKey: ((x: any) => any) | string): any {
     if (typeof(inputFunctionOrKey) == 'string') {
         return inputArray.map((x: any) => x[inputFunctionOrKey]).reduce((max: any, x: any) => max < x ? x : max)
     }

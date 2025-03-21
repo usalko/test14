6e40c87c17217482f91b35affacf26cd7ceafd47
Vanya Usalko <dk4aol@gmail.com>
2025-03-21 12:11:08 +0300

    # There is the implementation for previous task.

--- src/example251.ts
+++ src/example251.ts
@@ -3,6 +3,12 @@
 // object associating the properties to the values
 // of a given array of valid property identifiers and an array of values.
 export function example251(inputKeys: any[], inputValues: any[]): any {
-    return {}
+    return inputKeys.slice(0, Math.min(inputKeys.length, inputValues.length)).reduce(
+        (result: any, key: any, i: number) => {
+            result[key] = inputValues[i]
+            return result
+        },
+        {},
+    )
 }
 

6e6587e8537e8dbe05213344f615fe9d6ebe5af6
Vanya Usalko <dk4aol@gmail.com>
2025-03-23 09:52:33 +0300

    # There is the implementation for test 252.

--- src/example252.ts
+++ src/example252.ts
@@ -2,7 +2,13 @@
 
 // create an array of elements, grouped based on the position in the original array.
 export function example252(inputArray1: any[], inputArray2: any[], inputArray3: any[]): any[] {
-    return []
+    return [...Array(Math.max(inputArray1.length, inputArray2.length, inputArray3.length))].map((item: any, i: number) => {
+            return [
+            i < inputArray1.length? inputArray1[i]: null,
+            i < inputArray2.length? inputArray2[i]: null,
+            i < inputArray3.length? inputArray3[i]: null,
+        ]
+    })
 }
 
 

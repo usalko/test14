c584e48f5b4d4706c3db4ca8ce360b06f283383c
Vanya Usalko <dk4aol@gmail.com>
2025-05-10 23:56:17 +0300

    # There is the new one task.

---
+++ src/example301.ts
@@ -0,0 +1,8 @@
+// src/example301.ts
+import {readFileSync} from 'fs'
+
+// mutate the original array to filter out the values specified.
+// Returns the removed elements.
+export function example301(inputArray: any[], inputFilter: any[]): any[] {
+    return []
+}
---
+++ tests/example301.spec.ts
@@ -0,0 +1,26 @@
+// tests/example301.spec.ts
+import { expect } from 'chai'
+import { example301 } from '../src/example301'
+import { writeFileSync } from 'fs';
+
+
+console.log(`Fundamental(ES6)-Part-I
+161. Filter and Return Removed Elements
+
+
+Write a JavaScript program to 'mutate the original array to filter out the values specified.
+Returns the removed elements.'
+
+
+ - Use Array.prototype.filter() and Array.prototype.includes() to pull out the values that are not needed.
+ - Set Array.prototype.length to mu tate the passed in an array by resetting its length to 0.
+ - Use Array.prototype.push() to re-populate it with only the pulled values.
+ - Use Array.prototype.push() to keep track of pulled values.
+
+`)
+
+describe('#example301', () => {
+    it(`Input arguments are ['a', 'b', 'c', 'd'], ['b', 'd'] #1`, () => {
+        expect(example301(['a', 'b', 'c', 'd'], ['b', 'd'])).deep.equal(['b', 'd'])
+    })
+})

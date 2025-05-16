e1a1a16afa3ffff7377864f4769a892fb4aab9e2
Vanya Usalko <dk4aol@gmail.com>
2025-05-16 17:13:16 +0300

    # There is the fix call for async test.

--- tests/example306.spec.ts
+++ tests/example306.spec.ts
@@ -17,11 +17,12 @@ Write a JavaScript program to perform left-to-right function composition for asy
 `)
 
 describe('#example306', () => {
-    it(`Input arguments are x => x + 1, x => new Promise(resolve => setTimeout(() => resolve(x + 2), 1000)), x => x + 3, async x => (await x) + 4  #1`, () => {
-        expect((await example306(
-            (x: any) => x + 1,
-            (x: any) => new Promise(resolve => setTimeout(() => resolve(x + 2), 1000)),
-            (x: any) => x + 3,
-            async (x: any) => (await x) + 4))(5)).equal(15)
-    })
+    it(`Input arguments are x => x + 1, x => new Promise(resolve => setTimeout(() => resolve(x + 2), 1000)), x => x + 3, async x => (await x) + 4  #1`,
+        async () => {
+            expect((await example306(
+                (x: any) => x + 1,
+                (x: any) => new Promise(resolve => setTimeout(() => resolve(x + 2), 1000)),
+                (x: any) => x + 3,
+                async (x: any) => (await x) + 4))(5)).equal(15)
+        })
 })

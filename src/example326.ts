a51d2e221af216f3a29484af80a060db403c3381
Vanya Usalko <dk4aol@gmail.com>
2025-06-09 08:44:13 +0300

    # There is the new one test.

---
+++ src/example326.ts
@@ -0,0 +1,6 @@
+// src/example326.ts
+
+// check if the given argument is a symbol.
+export function example326(inputArgument: any): boolean {
+    return typeof(inputArgument) === 'symbol'
+}
---
+++ tests/example326.spec.ts
@@ -0,0 +1,29 @@
+// tests/example326.spec.ts
+import { expect } from 'chai'
+import { example326, example326v2 } from '../src/example326'
+
+
+console.log(`Fundamental(ES6)-Part-I
+186. Check If Symbol
+
+
+Write a JavaScript program to check if the given argument is a symbol.
+
+
+ - Use typeof to check if a value is classified as a symbol primitive.
+`)
+
+describe('#example326', () => {
+    it(`Input argument is Symbol('x') #1`, () => {
+        expect(example326(Symbol('x'))).equal(true)
+    })
+    it(`Input argument is Symbol(1) #2`, () => {
+        expect(example326(Symbol(1))).equal(true)
+    })
+    it(`Input argument is Symbol(')') #3`, () => {
+        expect(example326(Symbol(')'))).equal(true)
+    })
+    it(`Input argument is null #4`, () => {
+        expect(example326(null)).equal(false)
+    })
+})

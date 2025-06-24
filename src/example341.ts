19ea6a03e9a3609582729f191535d3203d455d95
Vanya Usalko <ivict@rambler.ru>
2025-06-24 19:23:07 +0300

    # There is the new one test.

---
+++ src/example341.ts
@@ -0,0 +1,6 @@
+// src/example341.ts
+
+// check if the first numerical argument is divisible by the second one.
+export function example341(inputArgument1: number, inputArgument2: number): boolean {
+    return inputArgument1 % inputArgument2 === 0
+}
---
+++ tests/example341.spec.ts
@@ -0,0 +1,31 @@
+// tests/example341.spec.ts
+import { expect } from 'chai'
+import { example341 } from '../src/example341'
+
+
+console.log(`Fundamental(ES6)-Part-II
+201. Check Divisibility
+
+
+Write a JavaScript program to check if the first numerical argument is divisible by the second one.
+
+
+ - Use the modulo operator (%) to check if the remainder is equal to 0.
+
+
+`)
+
+describe('#example341', () => {
+    it(`Input argument are 6, 3 #1`, () => {
+        expect(example341(6, 3)).equal(true)
+    })
+    it(`Input argument are 5, 3 #2`, () => {
+        expect(example341(5, 3)).equal(false)
+    })
+    it(`Input argument are 100, 10 #2`, () => {
+        expect(example341(100, 10)).equal(true)
+    })
+    it(`Input argument are 0, 3 #2`, () => {
+        expect(example341(0, 3)).equal(true)
+    })
+})

7f75bd7085bdaee6c2cc66c97d5c2fd2935bf99e
Vanya Usalko <dk4aol@gmail.com>
2025-07-19 15:28:27 +0300

    # There is the test for Array.from().map() construction. (alternative to use new Array(n).fill()
    Signed-off-by: Vanya Usalko <dk4aol@gmail.com>

--- src/example352.ts
+++ src/example352.ts
@@ -6,3 +6,10 @@ export function example352(inputElement1: any, ...inputDimensionSize: number[]):
         inputElement1 :
         new Array(inputDimensionSize[0]).fill(example352(inputElement1, ...inputDimensionSize.slice(1)))
 }
+
+// create a n-dimensional array with a given value.
+export function example352_2(inputElement1: any, ...inputDimensionSize: number[]): any {
+    return inputDimensionSize.length === 0 ?
+        inputElement1 :
+        Array.from({length: inputDimensionSize[0]}).map(() => example352_2(inputElement1, ...inputDimensionSize.slice(1)))
+}
--- tests/example352.spec.ts
+++ tests/example352.spec.ts
@@ -1,6 +1,6 @@
 // tests/example352.spec.ts
 import { expect } from 'chai'
-import { example352 } from '../src/example352'
+import { example352, example352_2 } from '../src/example352'
 
 
 console.log(`Fundamental(ES6)-Part-II
@@ -21,5 +21,11 @@ describe('#example352', () => {
     })
     it(`Input arguments are 5, 2, 2, 2 #1`, () => {
         expect(example352(5, 2, 2, 2)).deep.equal([[[5, 5], [5, 5]], [[5, 5], [5, 5]]])
+    })
+    it(`Input arguments are 1, 3 #1`, () => {
+        expect(example352_2(1, 3)).deep.equal([1, 1, 1])
+    })
+    it(`Input arguments are 5, 2, 2, 2 #1`, () => {
+        expect(example352_2(5, 2, 2, 2)).deep.equal([[[5, 5], [5, 5]], [[5, 5], [5, 5]]])
     })
 })

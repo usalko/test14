ea9068556ef484fb0884b00dfa0dfc65b2f19a55
Vanya Usalko <dk4aol@gmail.com>
2025-07-19 15:16:15 +0300

    # There is the new one test.

---
+++ src/example352.ts
@@ -0,0 +1,8 @@
+// src/example352.ts
+
+// create a n-dimensional array with a given value.
+export function example352(inputElement1: any, ...inputDimensionSize: number[]): any {
+    return inputDimensionSize.length === 0 ?
+        inputElement1 :
+        new Array(inputDimensionSize[0]).fill(example352(inputElement1, ...inputDimensionSize.slice(1)))
+}
---
+++ tests/example352.spec.ts
@@ -0,0 +1,25 @@
+// tests/example352.spec.ts
+import { expect } from 'chai'
+import { example352 } from '../src/example352'
+
+
+console.log(`Fundamental(ES6)-Part-II
+212. n-Dimensional Array
+
+
+Write a JavaScript program to create a n-dimensional array with a given value.
+
+
+ - Use recursion.
+ - Use Array.from(), Array.prototype.map() to generate rows where each is a new array initialized using initializeNDArray().
+
+`)
+
+describe('#example352', () => {
+    it(`Input arguments are 1, 3 #1`, () => {
+        expect(example352(1, 3)).deep.equal([1, 1, 1])
+    })
+    it(`Input arguments are 5, 2, 2, 2 #1`, () => {
+        expect(example352(5, 2, 2, 2)).deep.equal([[[5, 5], [5, 5]], [[5, 5], [5, 5]]])
+    })
+})

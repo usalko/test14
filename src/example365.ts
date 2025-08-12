057612f8002fa37b562d92380e28efb89ae01bdc
Vanya Usalko <dk4aol@gmail.com>
2025-08-13 00:00:46 +0300

    #  There i sthe fix.

--- src/example365.ts
+++ src/example365.ts
@@ -1,7 +1,7 @@
 // src/example365.ts
 
 // get the standard deviation of an array of numbers.
-export function example365(inputArray: number[], inputOption: boolean = false): bumber {
+export function example365(inputArray: number[], inputOption: boolean = false): number {
     return 0.
 }
 

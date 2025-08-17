b1cdbb7f4d53b39cf53a0a1045b8e6fbd29467c5
Vanya Usalko <dk4aol@gmail.com>
2025-08-17 16:06:24 +0300

    # There is the implementation of standard and sample deviation.

--- src/example365.ts
+++ src/example365.ts
@@ -1,7 +1,15 @@
 // src/example365.ts
 
 // get the standard deviation of an array of numbers.
-export function example365(inputArray: number[], inputOption: boolean = false): number {
-    return 0.
+export function example365(inputArray: number[], usePopulation: boolean = false): number {
+    if (inputArray.length < 2) {
+        return 0.
+    }
+    const sampleMean = inputArray.reduce((x: number, y: number) => x + y, 0) / inputArray.length
+    if (usePopulation) {
+        const mean = sampleMean
+        return Math.sqrt(inputArray.reduce((x: number, y: number) => x+ Math.pow(y - mean, 2), 0)/inputArray.length)
+    }
+    return Math.sqrt(inputArray.reduce((x: number, y: number) => x+ Math.pow(y - sampleMean, 2), 0)/(inputArray.length - 1))
 }
 

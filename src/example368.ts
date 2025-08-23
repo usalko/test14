4f9e4720787ed152a409edb437c441791102f44c
Vanya Usalko <dk4aol@gmail.com>
2025-08-23 15:26:15 +0300

    # There is the new one test.

---
+++ src/example368.ts
@@ -0,0 +1,9 @@
+// src/example368.ts
+
+import { debug } from "console";
+
+// log the name of a function.
+export function example368(inputFunction: any): string {
+    return inputFunction.name
+}
+
---
+++ tests/example368.spec.ts
@@ -0,0 +1,23 @@
+// tests/example368.spec.ts
+import { expect } from 'chai'
+import { example368, } from '../src/example368'
+
+
+console.log(`Fundamental(ES6)-Part-II
+228. Log Function Name
+
+
+Write a JavaScript program to log the name of a function.
+
+ 
+ - Use console.debug() and the name property of the passed function to log the function's name to the debug channel of the console.
+ - Return the given function fn.
+
+
+`)
+
+describe('#example368', () => {
+    it(`Input argument id: Math.max #1`, () => {
+        expect(example368(Math.max)).equal('max')
+    })
+})

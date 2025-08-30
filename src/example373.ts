0510c11983e4b06b8153c7ab5fea21cab9515b66
Vanya Usalko <dk4aol@gmail.com>
2025-08-30 03:17:02 +0300

    # There is the implementation of flattened object.

--- src/example373.ts
+++ src/example373.ts
@@ -1,7 +1,15 @@
 // src/example373.ts
 
 // flatten an object with the paths for keys.
-export function example373(inputObject: any, prefix: string = ''): any {
-    return Object.keys(inputObject)
+export function example373(inputObject: any, prefix: string = '', result: any = {}): any {
+    return Object.keys(inputObject).reduce((x: any, key: string) => {
+        const flattenedKey = prefix ? prefix + '.' + key: key
+        if (typeof(inputObject[key]) === 'object' && inputObject[key] !== null) {
+            Object.assign(x, example373(inputObject[key], flattenedKey), result)
+        } else {
+            x[flattenedKey] = inputObject[key]
+        }
+        return result
+    }, result)
 }
 

599a7b23f0a12193f7b1c9621cbca5bda299f9a4
Vanya Usalko <dk4aol@gmail.com>
2025-09-13 06:10:24 +0300

    # There is the little bit optimization.

--- src/example381.ts
+++ src/example381.ts
@@ -3,14 +3,15 @@
 // remove elements from an array until the passed function returns true.
 // Returns the remaining elements in the array.
 export function example381(inputArray1: any[], inputSaveCriteria: (x: any) => boolean): any[] {
-    let result = []
+    let sliceIndex = -1
     for (let i = 0; i < inputArray1.length; i++) {
         if (!inputSaveCriteria(inputArray1[i])) {
-            result = inputArray1.slice(i+1)
+            sliceIndex = i+1
         } else {
             break
         }
     }
-    return result
+
+    return sliceIndex === -1 ? inputArray1 : inputArray1.slice(sliceIndex)
 }
 

292a676d4b07b05ab69418116e721d652061a5cf
Vanya Usalko <dk4aol@gmail.com>
2025-11-04 03:42:26 +0300

    # There is the one new task.

---
+++ src/example385.ts
@@ -0,0 +1,7 @@
+// src/example385.ts
+
+// invokes the provided function after a few milliseconds
+export function example385(timeout: number, fn: any): any {
+    return setTimeout(timeout, )
+}
+
---
+++ tests/example385.spec.ts
@@ -0,0 +1,22 @@
+// tests/example385.spec.ts
+import { expect } from 'chai'
+import { example385, } from '../src/example385'
+
+
+console.log(`Fundamental(ES6)-Part-II
+245. Invoke Function After Delay
+
+Write a JavaScript program that invokes the provided function after a few milliseconds.
+
+ - Use setTimeout() to delay execution of fn.
+ - Use the spread (...) operator to supply the function with an arbitrary number of arguments.
+
+`)
+
+describe('#example385', () => {
+    // Call the 'delay' function with a function to be executed, a delay of 1000ms, and additional arguments
+    it(`Input arguments 1000, () => string #1`, () => {
+        const currentTimeMillis = Date.now()
+        expect(example385(1000, () => 'later')).equal('later')
+    })
+})
